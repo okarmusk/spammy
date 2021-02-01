b9b2ec300edb3662201fb4feb903d5dfcee642e2
Konrad Okarmus <okarmusk@outlook.com>
2021-02-01 21:42:26 +0100

    Logger fix

--- src/logger.js
+++ src/logger.js
@@ -1,11 +1,17 @@
-const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
 
-const configuration = {
-  transports: [
-    new winston.transports.Console()
-  ]
-};
+const logFormat = printf(({ level, message, label, timestamp }) => {
+  return `{ "timestamp": "${timestamp}", "level": "${level}", "message": "${message}" }`;
+});
 
-const logger = winston.createLogger(configuration);
+const logger = createLogger({
+  format: combine(
+    label(),
+    timestamp(),
+    logFormat
+  ),
+  transports: [new transports.Console()]
+});
 
 exports.logger = logger;
